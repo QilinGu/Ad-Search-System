bce777609b23d179abc124bcf7ea3547780c8cc3
chengchen886 <charlie@ccs.neu.edu>
2016-12-30 16:26:29 +0800

    product dao impl

--- Crawler/src/main/java/com/charles/crawler/model/dao/implement/ProductDAOImpl.java
+++ Crawler/src/main/java/com/charles/crawler/model/dao/implement/ProductDAOImpl.java
@@ -1,6 +1,7 @@
 package com.charles.crawler.model.dao.implement;
 
 import com.charles.crawler.model.dao.interfaces.ProductDAO;
+import com.charles.crawler.model.dao.rowmapper.ProductRowMapper;
 import com.charles.crawler.model.entity.Product;
 import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -44,18 +45,41 @@ public class ProductDAOImpl implements ProductDAO {
                                 return products.size();
                             }
                         });
+
+        log.info("->-> Ending method ProductDAOImpl.insert()");
+
         return updateCounts;
     }
 
     public void insert(Product product) {
+        log.debug("->-> Starting method ProductDAOImpl.insert()");
 
+        String insertSql = "INSERT INTO PRODUCT (URL, TITLE, PRICE, DESCRIPTION) VALUES(?,?,?,?);";
+
+        getJdbcTemplate().update(insertSql, new Object[]{product.getUrl(), product.getTitle(),
+                                                         product.getPrice(), product.getDescription()});
+        log.info("->-> Ending method ProductDAOImpl.insert()");
     }
 
+    @SuppressWarnings("unchecked")
+
     public List<Product> selectAll() {
-        return null;
+        log.debug("->-> Starting method ProductDAOImpl.selectAll()");
+
+        String selectAllSql = "SELECT * FROM PRODUCT;";
+
+        List<Product> productLists = getJdbcTemplate().query(selectAllSql, new ProductRowMapper());
+
+        log.info("->-> Ending method ProductDAOImpl.selectAll()");
+
+        return productLists;
     }
 
     public Product findById(String id) {
-        return null;
+        String findByIdSql = "SELECT * FROM PRODUCT WHERE TITLE =?;";
+
+        Product product = (Product) jdbcTemplate.queryForObject(findByIdSql, new Object[]{id}, new ProductRowMapper());
+
+        return product;
     }
 }
---
+++ Crawler/src/main/java/com/charles/crawler/model/dao/rowmapper/ProductRowMapper.java
@@ -0,0 +1,17 @@
+package com.charles.crawler.model.dao.rowmapper;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Created by ChenCheng on 12/30/2016.
+ */
+public class ProductRowMapper implements RowMapper{
+
+
+    public Object mapRow(ResultSet resultSet, int i) throws SQLException {
+        return null;
+    }
+}
